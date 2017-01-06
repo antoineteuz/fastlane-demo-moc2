d8e93b3367ff0964ee51a4b718166163d184afa2
Benoit BRIATTE <briatte.benoit@digipolitan.com>
2017-01-06 10:11:24 +0100

    add existing xcodeproj

---
+++ FastlaneDemoMoc2/FastlaneDemoMoc2.h
@@ -0,0 +1,19 @@
+//
+//  FastlaneDemoMoc2.h
+//  FastlaneDemoMoc2
+//
+//  Created by Benoit BRIATTE on 06/01/2017.
+//  Copyright © 2017 Digipolitan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FastlaneDemoMoc2.
+FOUNDATION_EXPORT double FastlaneDemoMoc2VersionNumber;
+
+//! Project version string for FastlaneDemoMoc2.
+FOUNDATION_EXPORT const unsigned char FastlaneDemoMoc2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FastlaneDemoMoc2/PublicHeader.h>
+
+
